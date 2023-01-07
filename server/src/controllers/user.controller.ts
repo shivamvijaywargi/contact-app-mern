04efaf61d8e6e1f015696d21969ae56b14111c87
Shivam Vijaywargi <vjshivam5@gmail.com>
2023-01-07 17:48:56 +0530

    BE: Created custom asyncHandler

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,20 +1,25 @@
 import { NextFunction, Request, Response } from "express";
+import asyncHandler from "../middlewares/asyncHandler.middleware";
 import User from "../models/User.model";
 import AppErr from "../utils/AppErr";
 
-export const getAllUsers = async (
-  _req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const users = await User.find({});
+/**
+ * @GET_ALL_USERS
+ * @ROUTE @GET {{URL}}/api/v1/users
+ * @returns All found Users
+ * @ACCESS Private (Admin only)
+ */
+export const getAllUsers = asyncHandler(
+  async (_req: Request, res: Response, next: NextFunction) => {
+    const users = await User.find({});
 
-  if (!users.length) {
-    return next(new AppErr("No user found", 404));
+    if (!users.length) {
+      return next(new AppErr("No user found", 404));
+    }
+
+    res.status(200).json({
+      success: true,
+      users,
+    });
   }
-
-  res.status(200).json({
-    success: true,
-    users,
-  });
-};
+);
