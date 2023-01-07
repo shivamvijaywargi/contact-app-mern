2e5acb5c304a365717da87e0518f0a53a04cbbe3
Shivam Vijaywargi <vjshivam5@gmail.com>
2023-01-07 14:57:17 +0530

    BE: Configure middlewares and setup Winston logger

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,40 @@
 import express from "express";
 import { config } from "dotenv";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import cors from "cors";
 
 config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello from backend");
+// Middlewares
+// Built-in
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// Third-party
+app.use(cookieParser());
+app.use(morgan("dev"));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
+
+/**
+ * @SERVER_STATUS
+ * @ROUTE @GET {{URL}}/api/ping
+ * @DESC Returns response 200 with message pong if api is working
+ * @ACCESS Public
+ */
+app.get("/api/ping", (_req, res) => {
+  res.status(200).json({
+    success: true,
+    status: "UP",
+    message: "PONG",
+  });
 });
 
 export default app;
